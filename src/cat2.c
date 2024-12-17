7dffa43afd7c134d3f6612bef9da24dbae920387
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-12-17 17:52:09 +0900

    [upd] cat2

--- src/cat2.c
+++ src/cat2.c
@@ -3,13 +3,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-  FILE* f = stdin;
+static void do_cat(FILE* f) {
   int c;
 
   while ((c = fgetc(f)) != EOF) {
     if (putchar(c) < 0) {
       exit(1);
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  FILE* f = stdin;
+  int c;
+
+  if (argc < 2) {
+    do_cat(f);
+    exit(0);
+  } else {
+    for (int i = 1; i < argc; i++) {
+      f = fopen(argv[i], "r");
+      if (!f) {
+        perror(argv[i]);
+        exit(1);
+      }
+      do_cat(f);
     }
   }
 
